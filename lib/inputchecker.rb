5728ca92588baa3847e688fb640e035dd1c4582e
Xavier Canal i Masjuan <xavi@itnig.net>
2012-05-28 12:11:35 +0200

    Method visible in models. No usefull behavior

--- lib/inputchecker.rb
+++ lib/inputchecker.rb
@@ -1,5 +1,12 @@
 require "inputchecker/version"
+require "active_model"
 
 module Inputchecker
-  # Your code goes here...
+  class EmailValidator < ActiveModel::EachValidator
+    def validate_each(record, attribute, value)
+      record.errors.add(attribute, "wrong, wrong, wrong")
+    end
+  end
 end
+
+ActiveModel::Validations.send(:include, Inputchecker)
