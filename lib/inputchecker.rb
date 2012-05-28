a64ca41d59214b484638446b2bd2a77329161bd3
Xavier Canal i Masjuan <xavi@itnig.net>
2012-05-28 16:54:53 +0200

    Basic test over email checker

--- lib/inputchecker.rb
+++ lib/inputchecker.rb
@@ -4,8 +4,15 @@ require "active_model"
 module Inputchecker
   class EmailValidator < ActiveModel::EachValidator
     def validate_each(record, attribute, value)
-      record.errors.add(attribute, "wrong, wrong, wrong")
+      record.errors.add(attribute, I18n.t("inputchecker.mail.wrong_format")) unless valid_email?(value)
     end
+
+    private
+
+    def valid_email?(value)
+      /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i.match(value).present?
+    end
+
   end
 end
 
--- spec/lib/input_checker_spec.rb
+++ spec/lib/input_checker_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
+class Test
+  include ActiveModel::Validations
+
+  validates :testfield, :email => true
+
+  attr_accessor :testfield
+end
+
 describe Inputchecker do
-  it "should be executable"
+  subject { Test.new }
+
+  context "when input is e-mail" do
+    it "should not allow value without @" do
+      subject.testfield = "itnig.net"
+      subject.should be_invalid
+    end
+
+    it "should not allow value without ." do
+      subject.testfield = "xavi@itnignet"
+      subject.should be_invalid
+    end
+
+    it "should not allow value without both . and @" do
+      subject.testfield = "xaviitnignet"
+      subject.should be_invalid
+    end
+
+    it "should allow value with e-mail format" do
+      subject.testfield = "xavi@itnig.net"
+      subject.should be_valid
+    end
+  end
 end
